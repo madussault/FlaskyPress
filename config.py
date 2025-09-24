8c6d964d66deae3480f6eaee328f5050a886cf1f
madussault <55608358+madussault@users.noreply.github.com>
2025-09-24 14:30:25 -0400

    Update config.py
    Rewrote the documentation to improve clarity

--- config.py
+++ config.py
@@ -5,83 +5,111 @@ from dotenv import load_dotenv
 
 
 basedir = os.path.abspath(os.path.dirname(__file__))
-# Will load the environment variables from the .env file that will
-# then be assigned to the attributes in our Config class. If we store
-# these config parameters in a .env file it is to prevent them from
-# being uploaded to github by accident.
 load_dotenv(os.path.join(basedir, '.env'))
 
 
 class Config:
-    """ Contains the config parameters that will be used by our app.
+"""
+FlaskyPress Configuration Settings
+----------------------------------
 
-        These parameters will be used by flask and it's extensions. Some of
-        them will also be used with modules unrelated to flask, for example
-        the logging module used to send crash reports by email.
+This file contains all configuration variables for the FlaskyPress application.
 
-        Attributes
-        ----------
-        SECRET_KEY : str
-            Input your own unique and secret key here. Will protect web forms
-            against CSRF attack and is also used to signs the cookies
-            cryptographically.
-        SQLALCHEMY_DATABASE_URI : str
-            Path to the database.
-        SQLALCHEMY_TRACK_MODIFICATIONS : bol
-            Flask-SQLAlchemy has its own event notification system that gets
-            layered on top of SQLAlchemy. It takes extra resource and we
-            don't need it.
-        SITE_NAME : str
-            Will show up as site branding in the menu.
+Core Settings:
+    SECRET_KEY (str)
+        A unique, secret value used to:
+        - Protect web forms from CSRF attacks.
+        - Cryptographically sign cookies for added security.
 
-            Will also be used by the mail logger to identify our app when an
-            error message is being sent.
-        POSTS_PER_PAGE : int
-            Maximum post per page to show on the /index, /drafts and /search
-            pages.
-        STYLE_EMBED : bol
-            If set to True will wrap embeds with bootstrap 4 classes and make
-            them responsive.
-        COPYRIGHT : str
-            Copyright info that will be shown in the footer.
-        URL_PREFIX : str
-            Only needed when we want to run our app from a domain subdirectory,
-            ex: www.my_website.com/subfolder/ instead of www.my_website.com .
-        SESSION_TYPE : str
-            Command the type of session interface the Flask-session extension
-            is going to use.
-        WHOOSHEE_DIR : str
-            Folder to store the search index. (Default to
-            app_root_folder/whooshee.)
-        WHOOSHEE_MIN_STRING_LEN : int
-            Min. characters for the search string (defaults to 3)
-        WHOOSHEE_WRITER_TIMEOUT : int
-            How long should whoosh try to acquire write lock? (defaults to 2)
-        WHOOSHEE_MEMORY_STORAGE : bol
-            Use the memory as storage instead of writing the search index to
-            file. Will be set to True for tests. (defaults to False)
-        WHOOSHEE_ENABLE_INDEXING : bol
-            Specify whether or not to actually do any operations with the
-            Whoosh index (defaults to True).
-        MAIL_SERVER : str
-            Address of the email server our mail logger is going to use to send
-            our reports in case of error (ex, smtp.googlemail.com). If
-            this value is not set the mail logger will be disabled.
-        MAIL_PORT : int
-            Port used by the mail server of your email service provider.
-            Consult their website to find out.
-        MAIL_USE_TLS : bol
-            Enable encryption when sending email, which prevent eavesdropping.
-        MAIL_USERNAME : str
-            Username for the email account sending the error reports.
-        MAIL_PASSWORD : str
-            Password for the email account sending the error reports.
-        ADMINS : list
-            Here we store a list of the email addresses that will receive error
-             reports, so your own email address should be in that list.
-        FROM_ADDRESS : str
-            Email address of the sender.
-    """
+    SQLALCHEMY_DATABASE_URI (str)
+        The database connection URL.
+        Defaults to SQLite but can be configured to use PostgreSQL, MySQL,
+        or any database supported by SQLAlchemy.
+
+    SQLALCHEMY_TRACK_MODIFICATIONS (bool)
+        Enables or disables SQLAlchemy's event notification system.
+        This feature consumes extra resources and is generally unnecessary,
+        so it should typically be set to False.
+
+Site and Content Settings:
+    SITE_NAME (str)
+        The name of your site:
+        - Displayed as branding in the navigation menu.
+        - Used by the mail logger to identify the app in error reports.
+
+    POSTS_PER_PAGE (int)
+        The maximum number of posts displayed per page on the /index, /drafts,
+        and /search pages.
+
+    STYLE_EMBED (bool)
+        When set to True, embeds are wrapped with Bootstrap 4 classes to make
+        them responsive.
+
+    COPYRIGHT (str)
+        The copyright information displayed in the site footer.
+
+    URL_PREFIX (str)
+        Required only when running FlaskyPress from a subdirectory of your domain.
+        Example:
+            Root domain: www.mywebsite.com
+            Subdirectory: www.mywebsite.com/subfolder/
+
+Session Settings:
+    SESSION_TYPE (str)
+        Specifies the session interface type used by Flask-Session.
+        Example: "filesystem", "redis", etc.
+
+Search Configuration (Whooshee):
+    WHOOSHEE_DIR (str)
+        Directory where the search index is stored.
+        Defaults to: app_root_folder/whooshee
+
+    WHOOSHEE_MIN_STRING_LEN (int)
+        Minimum number of characters required for a search query.
+        Default: 3
+
+    WHOOSHEE_WRITER_TIMEOUT (int)
+        Time (in seconds) Whooshee will try to acquire a write lock.
+        Default: 2
+
+    WHOOSHEE_MEMORY_STORAGE (bool)
+        Stores the search index in memory instead of writing it to a file.
+        Typically set to True only during testing.
+        Default: False
+
+    WHOOSHEE_ENABLE_INDEXING (bool)
+        Enables or disables search indexing operations.
+        Default: True
+
+Email Configuration (Error Reporting):
+    These settings are only required if you want to receive email notifications
+    when the application encounters errors. If MAIL_SERVER is not set, the mail
+    logger will be disabled.
+
+    MAIL_SERVER (str)
+        The SMTP server address used to send error reports.
+        Example: smtp.googlemail.com
+
+    MAIL_PORT (int)
+        The SMTP port used by your email service provider.
+        Refer to your provider's documentation for the correct value.
+
+    MAIL_USE_TLS (bool)
+        Enables TLS encryption for outgoing emails to prevent eavesdropping.
+
+    MAIL_USERNAME (str)
+        The username for the email account that sends error reports.
+
+    MAIL_PASSWORD (str)
+        The password for the email account that sends error reports.
+
+    ADMINS (list)
+        A list of email addresses that should receive error notifications.
+        Include your own address to ensure you receive alerts.
+
+    FROM_ADDRESS (str)
+        The email address displayed as the sender of error reports.
+"""
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
     SQLALCHEMY_DATABASE_URI = (os.environ.get('DATABASE_URL') or
                                'sqlite:///' + os.path.join(basedir, 'blog.db'))
